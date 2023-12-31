ccbe430c7d1dfff0464082671f1dcf61d8c5462e
Allen <allenshamrock37@gmail.com>
2023-12-31 16:33:50 +0300

    Fixed the user and repo bug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetchUser(input.value);
   //displayRepo(input.value);
-  form.reset()
+  form.reset();
 });
 
 function fetchUser(userName) {
@@ -17,56 +17,36 @@ function fetchUser(userName) {
       let userCard = document.createElement("div");
       userCard.className = "users";
       userCard.innerHTML = `
+
    <div class ="user">
       <img src="${data.avatar_url}" alt="${data.login}"/>
           <span class="username">${data.login}</span>
-   </div>
-          <div id="repository">
 
-          </div>`;
-          let repos = "";
-          let reposurl = data.repos_url;
-          fetch(reposurl)
-            .then((response) => response.json())
-            .then((data) => {
-              // console.log(data[0].name);
-              for (i = 0; i < data.length; i++) {
-                repos += `<ul>
+   </div>
+   <div id="repository">
+   </div>
+`;
+      let repos = "";
+      let reposurl = data.repos_url;
+      fetch(reposurl)
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log(data[0].name);
+          for (i = 0; i < data.length; i++) {
+            repos += `<ul>
                   <li>
                   <a href="${data[i].html_url}">${data[i].name} </a>
                   </li>
                 </ul>`;
-                // console.log(repos)
-                document.querySelector("#repository").innerHTML = `${repos}`;
-              }
-            });
-          
-
+            // console.log(repos)
+          }
+          document.querySelector("#repository").innerHTML = `${repos}`;
+        });
+        let existingUser = document.querySelector('.users')
+        if(existingUser){
+          existingUser.remove()
+        }
       detailscard.appendChild(userCard);
     });
 }
-// function displayRepo(username) {
-//   fetch(`https://api.github.com/users/${username}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       //console.log(data.repos_url);
-//       let repos = "";
-//       let reposurl = data.repos_url;
-//       fetch(reposurl)
-//         .then((response) => response.json())
-//         .then((data) => {
-//           // console.log(data[0].name);
-//           for (i = 0; i < data.length; i++) {
-//             repos += `<ul></ul>
-//               <li>
-//               <a href="${data[i].html_url}">${data[i].name} </a>
-//               </li>
-//             </ul>`;
-//             // console.log(repos)
-//             document.querySelector("#repository").innerHTML = `${repos}`;
-//           }
-//         });
-//     });
-// }
 
-// for(const info of data)
